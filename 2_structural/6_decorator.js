4fd9f9776a9cef46119e45ed85aef7efe1020ca4
Corvax <vlvete1990@yandex.ru>
2020-05-13 15:32:17 +0300

    adding dp decorator

---
+++ 2_structural/6_decorator.js
@@ -0,0 +1,34 @@
+class Server {
+    constructor(ip, port){
+        this.ip = ip;
+        this.port = port;
+    }
+
+    get url() {
+        return `https://${this.ip}:${this.port}`
+    }
+
+}
+
+// decorator
+function aws(server) {
+    server.isAWS = true
+    server.awsInfo = function() {
+        return server.url;
+    }
+    return server;
+}
+
+function azure(server){
+    server.isAzure = true;
+    server.port += 500;
+    return server;
+}
+
+const s1 = aws(new Server('12.34.56.78', 8080));
+console.log(s1.isAWS);
+console.log(s1.awsInfo());
+
+const s2 = azure(new Server('98.87.76.78', 1000));
+console.log(s2.isAzure);
+console.log(s2.port);
